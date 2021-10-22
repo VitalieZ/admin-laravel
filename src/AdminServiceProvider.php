b97c5b94e13d6465a8178c3e7ffbc38a255917c4
VitalieZ <zaharcovitalie@yandex.ru>
2021-10-22 21:36:21 +0300

    by

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -38,7 +38,7 @@ class AdminServiceProvider extends ServiceProvider
         $this->publishes([__DIR__ . '/resources/assets' => public_path('assets'),], 'public');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations', 'admin');
         $this->publishes([
-            __DIR__ . '/database/migrations/create_admin_tables.php' => $this->getMigrationFileName('create_admin_tables.php'),
+            __DIR__ . '/database/migrations/create_admin_tables.php.stub' => $this->getMigrationFileName('create_admin_tables.php'),
         ], 'migrations');
 
         UiCommand::macro('admin', function (UiCommand $command) {
