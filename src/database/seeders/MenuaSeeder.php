2e6fc1f4a999de8d7ef5cf103061875115373b38
VitalieZ <zaharcovitalie@yandex.ru>
2021-11-07 19:25:58 +0200

    m

--- src/database/seeders/MenuaSeeder.php
+++ src/database/seeders/MenuaSeeder.php
@@ -4,8 +4,6 @@ namespace Viropanel\Admin\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class MenuaSeeder extends Seeder
 {
